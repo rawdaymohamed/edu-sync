04b6d0481abe13b3f1d62fab4e25490f5dc90d59
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-03-11 02:12:23 +0200

    Feat: Add admin dashboard design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,20 @@
+import UserCard from "@/app/components/UserCard";
 import React from "react";
 
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="flex flex-col md:flex-row p-4">
+      {/* Left */}
+      <div className="lg:w-2/3 flex flex-wrap gap-5   ">
+        <UserCard type="student" />
+        <UserCard type="teacher" />
+        <UserCard type="parent" />
+        <UserCard type="staff" />
+      </div>
+      {/* Right */}
+      <div className="lg:w-1/3">r</div>
+    </div>
+  );
 };
 
 export default AdminPage;
